d3eb8f07d63ae349cf11f2e5b04099c4777183fc
changwoo <changwoo@ivynet.co.kr>
2018-11-25 15:56:57 +0900

    'wp_user_level' 메타 값 추가.
    잘 사용되지는 않지만, author 메타박스에서 사용되는 항목이므로,
    유저 역할과 권한 추가시 넣어 줌.

---
+++ src/Initiators/RolesCapsInitiator.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Ivy\Maint\Initiators;
+
+use Ivy\Maint\Models\RolesCaps\IssueAgentModel;
+use Ivy\Maint\Models\RolesCaps\ProjectManagerModel;
+use Ivy\Maint\Models\RolesCaps\SupervisorModel;
+use Ivy\Mu\Initiators\AutoHookInitiator;
+
+class RolesCapsInitiator extends AutoHookInitiator
+{
+    /**
+     * @param array    $meta
+     * @param \WP_User $user
+     *
+     * @return array
+     */
+    public function filter_10_2_insert_user_meta($meta, $user)
+    {
+        $wpdb      = &muGetWpDb();
+        $userLevel = intval($user->user_level);
+
+        if ($userLevel < 7 && $user->has_cap(SupervisorModel::getRoleName())) {
+            $meta["{$wpdb->prefix}user_level"] = 7;
+        } elseif ($userLevel < 6 && $user->has_cap(ProjectManagerModel::getRoleName())) {
+            $meta["{$wpdb->prefix}user_level"] = 6;
+        } elseif ($userLevel < 5 && $user->has_cap(IssueAgentModel::getRoleName())) {
+            $meta["{$wpdb->prefix}user_level"] = 5;
+        }
+
+        return $meta;
+    }
+}
