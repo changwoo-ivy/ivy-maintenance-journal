d761ca30ad787878d55d85776456f577e4746adf
changwoo <changwoo@ivynet.co.kr>
2018-11-25 14:23:37 +0900

    수퍼바이저 역할에게 마일스톤 택소노미의 전권을 부여.

--- src/Models/RolesCaps/SupervisorModel.php
+++ src/Models/RolesCaps/SupervisorModel.php
@@ -4,6 +4,7 @@ namespace Ivy\Maint\Models\RolesCaps;
 
 use Ivy\Maint\Models\CustomPosts\IssueModel;
 use Ivy\Maint\Models\CustomPosts\ProjectModel;
+use Ivy\Maint\Models\Taxonomies\MilestoneModel;
 use Ivy\Mu\Models\BaseRolesCapsModel;
 use function Ivy\Maint\Functions\prefixed;
 
@@ -21,12 +22,14 @@ class SupervisorModel extends BaseRolesCapsModel
 
     public function getCapabilities()
     {
-        $projectCaps = array_values(ProjectModel::getCapabilityArray());
-        $issueCaps   = array_values(IssueModel::getCapabilityArray());
+        $projectCaps   = array_values(ProjectModel::getCapabilityArray());
+        $issueCaps     = array_values(IssueModel::getCapabilityArray());
+        $milestoneCaps = array_values(MilestoneModel::getCapabilityArray());
 
         return array_merge(
             array_combine($projectCaps, array_pad([], count($projectCaps), true)),
-            array_combine($issueCaps, array_pad([], count($issueCaps), true))
+            array_combine($issueCaps, array_pad([], count($issueCaps), true)),
+            array_combine($milestoneCaps, array_pad([], count($milestoneCaps), true))
         );
     }
 }
