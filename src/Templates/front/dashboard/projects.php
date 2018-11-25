d105213b26975e4573042501a699394a54fe6f95
changwoo <changwoo@ivynet.co.kr>
2018-11-25 21:10:03 +0900

    프로젝트 페이지의 목업 페이지 코드 작성.

---
+++ src/Templates/front/dashboard/projects.php
@@ -0,0 +1,62 @@
+<div class="row">
+  <div class="col-md-12">
+    <div class="bgc-white bd bdrs-3 p-20 mb-20">
+      <h4 class="c-grey-900 mb-20">당신의 프로젝트들</h4>
+      <p>회원님이 소속된 프로젝트의 목록이 나타납니다.</p>
+      <table id="projects-list" class="table table-hover">
+        <thead class="thead-dark">
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">이름</th>
+          <th scope="col">상태</th>
+          <th scope="col">담당 매니저</th>
+          <th scope="col">프로젝트 시작일</th>
+          <th scope="col">프로젝트 종료일</th>
+        </tr>
+        </thead>
+        <tbody class="table-striped">
+        <tr>
+          <th scope="row">28</th>
+          <td>호텔 마끼아또 개편 프로젝트</td>
+          <td><span class="badge badge-secondary p-5">대기중</span></td>
+          <td>심재완</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <th scope="row">25</th>
+          <td>삼성 소셜 마케팅 사이트 업데이트</td>
+          <td><span class="badge badge-info p-5">진행</span></td>
+          <td>심재완</td>
+          <td>2018년 11월 25일</td>
+          <td></td>
+        </tr>
+        <tr>
+          <th scope="row">22</th>
+          <td>라인 엔지니어링 블로그 업데이트</td>
+          <td><span class="badge badge-success p-5">종료</span></td>
+          <td>심재완</td>
+          <td>2018년 09월 01일</td>
+          <td>2018년 11월 10일</td>
+        </tr>
+        <tr>
+          <th scope="row">20</th>
+          <td>매거진 브리크</td>
+          <td><span class="badge badge-warning p-5">유지보수</span></td>
+          <td>심재완</td>
+          <td>2018년 09월 01일</td>
+          <td></td>
+        </tr>
+        <tr>
+          <th scope="row">15</th>
+          <td>진상 고객 프로젝트</td>
+          <td><span class="badge badge-danger p-5">취소</span></td>
+          <td>심재완</td>
+          <td>2018년 04월 20일</td>
+          <td>2018년 04월 30일</td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
